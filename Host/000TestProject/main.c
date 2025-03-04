a2cb769b3abf7a55545f1ae5c0412caa3b2072c7
rahulbari <rahulbari717@gmail.com>
2025-03-04 13:40:21 +0530

    Initial commit

---
+++ Host/000TestProject/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 4, 2025
+ *      Author: lenovo
+ */
+
+#include <stdio.h>
+
+int main(){
+
+	printf("Hello world\n");
+	printf("Press Enter to exit this application");
+	getchar();
+	return 0;
+}
---
+++ Target/001HelloWorld/Src/main.c
@@ -0,0 +1,11 @@
+/**
+
+ */
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void)
+{
+    printf("Hello rahul \n");
+	for(;;);
+}
